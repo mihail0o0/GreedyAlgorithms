6d3995cb41f1a90743cd4217e18c876c27b59d99
Mihailo Petrovic <mihailothemix@gmail.com>
2023-11-27 03:07:32 +0100

    Typescript setup

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function test(uloge) {
+    let index = -1;
+    for (let i = 0; i < uloge.length; i++) {
+    }
+    return index;
+}
+const testCause = ['p', 'l', 'l', 'p', 'l'];
+console.log(test(testCause));
---
+++ main.ts
@@ -0,0 +1,12 @@
+function test(uloge: string[]): number {
+    let index = -1;
+
+    for (let i = 0; i < uloge.length; i++) {
+         
+    }
+
+    return index;
+}
+
+const testCause = ['p', 'l', 'l', 'p', 'l'];
+console.log(test(testCause));
